73550df22a1bdeaea4c39ac6cb8d1462ece8c9c5
吴欢达 <wuhd01@mingyuanyun.com>
2021-03-08 17:50:39 +0800

    feat: 新增webhook脚本

---
+++ webhook.js
@@ -0,0 +1,44 @@
+
+let http = require('http');
+let crypto = require('crypto');
+var spawn = require('child_process').spawn;
+const SECRET = '123456';
+function sign (data) {
+  return 'sha1=' + crypto.createHmac('sha1', SECRET).update(data).digest('hex')
+}
+let server = http.createServer(function(req,res){
+  console.log(req.method,req.url);
+  if(req.url == '/webhook' && req.method =='POST'){
+    let buffers = []; 
+    req.on('data',function(data){
+      buffers.push(data);
+    });
+    req.on('end',function(){
+      let body = Buffer.concat(buffers);
+      let sig   = req.headers['x-hub-signature'];
+      let event = req.headers['x-github-event'];
+      let id    = req.headers['x-github-delivery'];
+      if(sig !== sign(body)){
+        return res.end('Not Allowed');
+      }
+      res.setHeader('Content-Type','application/json');
+      res.end(JSON.stringify({"ok":true}));
+      //===========开始部署ci/cd===================
+      if(event === 'push'){
+        let payload = JSON.parse(body);
+        let child = spawn('sh', [`./${payload.repository.name}.sh`]);
+        let buffers = [];
+        child.stdout.on('data', function (buffer) { buffers.push(buffer)});
+        child.stdout.on('end', function () {
+          let logs = Buffer.concat(buffers).toString();
+          console.log(logs)
+        });
+      }
+    });
+  }else{
+    res.end('Not Found!');
+  }
+});
+server.listen(4000,()=>{
+  console.log('服务正在4000端口上启动!');
+});
